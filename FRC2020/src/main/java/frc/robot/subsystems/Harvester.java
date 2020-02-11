1f518dd365816edc88f38eab3373a817bfb17045
maximilan <clemens@herfarth.com>
2020-02-11 13:57:35 -0500

    basic subsystem harvester

--- FRC2020/src/main/java/frc/robot/subsystems/Harvester.java
+++ FRC2020/src/main/java/frc/robot/subsystems/Harvester.java
@@ -8,6 +8,14 @@ public class Harvester extends Subsystem{
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
     }
+
+    public static void stop() {
+        //code for stopping harvester here
+    }
+
+    public static void run() {
+        //code for moving harvester here
+    }
     
 
 }
